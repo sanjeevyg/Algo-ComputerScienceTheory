b652be66aea1dd715eec2f079d28d5ab0d328647
sanjeevyg <yogi.sjv@gmail.com>
2022-02-14 22:51:21 -0500

    depthFirstvalues udpated

--- 4.BinaryTree.js
+++ 4.BinaryTree.js
@@ -23,12 +23,12 @@ const depthFirstValues = (root) => {
     return result  
 }
 
-// const a = new Node('a')
-// const b = new Node('b')
-// const c = new Node('c')
-// const d = new Node('d')
-// const e = new Node('e')
-// const f = new Node('f')
+const a = new Node('a')
+const b = new Node('b')
+const c = new Node('c')
+const d = new Node('d')
+const e = new Node('e')
+const f = new Node('f')
 
 const a = new Node(2)
 const b = new Node(5)
