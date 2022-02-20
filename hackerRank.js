08064e8ea25c5af815f42516a9305a2fda35b015
sanjeevyg <yogi.sjv@gmail.com>
2022-02-20 15:53:40 -0500

    miniMaxSum algo

--- hackerRank.js
+++ hackerRank.js
@@ -10,14 +10,28 @@ function plusMinus(arr) {
     let negative = 0;
     let zero = 0;
     for(let element of arr) {
-        if (element > 0) { positive++ }
-        if (element < 0) { negative++ }
-        if (element == 0) { zero++ }
+        if (element > 0) positive++ 
+        if (element < 0) negative++ 
+        if (element == 0) zero++ 
     }
     console.log(positive/s)
     console.log(negative/s)
     console.log(zero/s)
- }
+}
  
 
- plusMinus(array)
+plusMinus(array)
+
+let arr = [1, 2, 3, 4, 5]
+
+function miniMaxSum(arr) {
+    let sum = arr.reduce((sum, element) => sum + element)
+    let sortedArr = arr.sort((a, b) => a - b)
+    let maxSum = sum - sortedArr[0]
+    let minSum = sum - sortedArr[sortedArr.length - 1]
+    console.log(`${minSum} ${maxSum}`)   
+}
+
+
+miniMaxSum(arr)
+
