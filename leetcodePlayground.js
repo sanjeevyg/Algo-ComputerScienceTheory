33d7b037b2c0a7e410440e12f76309a2e197f173
sanjeevyg <yogi.sjv@gmail.com>
2022-02-26 23:07:40 -0500

    saturday aglo practice

--- leetcodePlayground.js
+++ leetcodePlayground.js
@@ -682,3 +682,103 @@ console.log(isPalindrome(-121))
 
 
 // Do it again without using converting to string
+
+
+
+// depth first-values;
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const depthFirstValues = (root) => {
+    if (root === null) return [];
+    const result = [];
+    const stack = [ root ]; 
+    while(stack.length > 0) {
+        const current = stack.pop()
+        // console.log(current.val);
+        result.push(current.val)
+
+        if (current.right) stack.push(current.right); 
+        if (current.left) stack.push(current.left);
+    }
+    return result  
+}
+
+const a = new Node('a')
+const b = new Node('b')
+const c = new Node('c')
+const d = new Node('d')
+const e = new Node('e')
+const f = new Node('f')
+
+const a = new Node(2)
+const b = new Node(5)
+const c = new Node(7)
+const d = new Node(9)
+const e = new Node(12)
+const f = new Node(14)
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+ console.log(depthFirstValues(a))
+
+
+ const depthFirstValues2nd = (root) => {
+    if(root === null) return [];
+    const leftValues = depthFirstValues2nd(root.left) 
+    const rightValues = depthFirstValues2nd(root.right) 
+    return [root.val, ...leftValues, ...rightValues]
+ }
+
+
+ //breath-first-values : 
+
+
+ const breadthFirstValues = ( root ) => {
+     let result = [];
+     if(root === null) return []
+     let queue = [root];
+     while(queue.length > 0) {
+         const current = queue.shift()
+         result.push(current.val)
+         if(current.left) queue.push(current.left)
+         if(current.right) queue.push(current.right)
+     }
+     return result
+     
+ }
+
+//  console.log(breadthFirstValues(a))
+
+
+ const treeIncludes = (target, root) => {
+     if(root === null) return false
+     let queue = [root]
+     while(queue.length > 0) {
+        const current = queue.shift();
+        if (current.val === target) return true
+        if(current.left) queue.push(current.left) 
+        if(current.right) queue.push(current.right) 
+     }
+     return false
+ }
+
+//  console.log(treeIncludes("e", a))
+
+
+ const treeIncludesRecursieveMethod = (target, root) => {
+     if(root === null) return false;
+     if(root.val === target) return true;
+     return treeIncludesRecursieveMethod(target, root.left) || treeIncludesRecursieveMethod(target, root.right)
+ }
+
+//  console.log(treeIncludesRecursieveMethod("e", a))
