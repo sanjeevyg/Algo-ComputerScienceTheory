8d3860964b69b90e6c50aded1cea279eed658c6a
sanjeevyg <yogi.sjv@gmail.com>
2022-04-17 23:47:12 -0400

    algo sun

--- NewAlgo/Repeat.js
+++ NewAlgo/Repeat.js
@@ -163,11 +163,27 @@ var mergeTwoLists = function(list1, list2) {
  
     return head.next
 }
-//                C1
-//           
-// 1 -> 2 -> 3 -> 5
-//           T
-//          C2
-// 3 -> 4 -> 6 -> 9 -> 12
 
-console.log(mergeTwoLists(a, d))
+
+var mergeTwoListsR = function(list1, list2) {
+  
+    if(list1 === null)  {
+        return list2
+    } else if(list2 === null) {
+        return list1
+    } else if( list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
+    }
+}
+
+
+
+
+
+
+
+console.log(mergeTwoListsR(a, d))
---
+++ NewAlgo/hello.rb
@@ -0,0 +1,5 @@
+def reverse_str(str)
+    return str.reverse
+end
+
+puts(reverse_str("Madam, I'm Adam"))
