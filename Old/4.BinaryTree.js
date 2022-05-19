87dce0b34e52b660c262813d43d20c96eab80813
sanjeevyg <yogi.sjv@gmail.com>
2022-05-18 23:38:05 -0400

    breadFirstValues

--- Old/4.BinaryTree.js
+++ Old/4.BinaryTree.js
@@ -192,3 +192,17 @@ const treeIncludes = (target, root) => {
 }
 
 //  console.log(treeIncludes("e", a))
+
+const breadthFirstValues = ( root ) => {
+    let result = [];
+    if(root === null) return []
+    let queue = [root];
+    while(queue.length > 0) {
+        const current = queue.shift()
+        result.push(current.val)
+        if(current.left) queue.push(current.left)
+        if(current.right) queue.push(current.right)
+    }
+    return result
+    
+}
