4cbd1dffe00091d58bf3657bf6c02eb20324189e
sanjeevyg <yogi.sjv@gmail.com>
2022-06-05 23:55:15 -0400

    mergeSort

--- Types of Sorts/MergeSort.js
+++ Types of Sorts/MergeSort.js
@@ -1,7 +1,24 @@
 function mergeSort(ar) {
-    //
+    //make a merger function 
+    
+
     
 }
 
+const merge = (ar1, ar2) => {
+    let newArr = [];
+    while(ar1.length > 0 || ar2.length > 0) {
+        if(ar1[ar1.length - 1] < ar2[ar2.length -1]) {
+            newArr.push(ar1.pop())
+        } else {
+            newArr.push(ar2.pop())
+        }
+    }
 
-console.log(mergeSort([1, 3, 7, 3, 2, 14, 12, 27, 33, 5, 1, 19]))
+    return [...newArr, ...ar1, ...ar2]
+}
+
+console.log(merge([1, 2, 3], [ 0, 2, 5]))
+
+
+// console.log(mergeSort([1, 3, 7, 3, 2, 14, 12, 27, 33, 5, 1, 19]))
