032371a3ff03cddecace5f4d84a7dae28477cddc
sanjeevyg <yogi.sjv@gmail.com>
2022-06-06 00:28:07 -0400

    merge sort

--- Types of Sorts/MergeSort.js
+++ Types of Sorts/MergeSort.js
@@ -1,24 +1,49 @@
 function mergeSort(ar) {
-    //make a merger function 
-    
-
-    
+    if(ar.length <= 1) return ar
+    let middle = Math.floor(ar.length/2);
+    let first = mergeSort(ar.slice(0, middle));
+    let second = mergeSort(ar.slice(middle));
+    return merge(first, second)
 }
 
 const merge = (ar1, ar2) => {
     let newArr = [];
-    while(ar1.length > 0 || ar2.length > 0) {
-        if(ar1[ar1.length - 1] < ar2[ar2.length -1]) {
-            newArr.push(ar1.pop())
+    while(ar1.length > 0 && ar2.length > 0) {
+        if(ar1[0] < ar2[0]) {
+            newArr.push(ar1.shift())
         } else {
-            newArr.push(ar2.pop())
+            newArr.push(ar2.shift())
         }
     }
-
     return [...newArr, ...ar1, ...ar2]
 }
+// const merge = (ar1, ar2) => {
+//     let newArr = [];
+//     let i = 0;
+//     let j = 0;
+//     while(i < ar1.length && j < ar2.length) {
+//         if(ar1[i] < ar2[j]) {
+//             newArr.push(ar1[i])
+//             i++
+//         } else {
+//             newArr.push(ar2[j])
+//             j++
+//         }
+//     }
 
-console.log(merge([1, 2, 3], [ 0, 2, 5]))
+//     while(i < ar1.length) {
+//         newArr.push(ar1[i])
+//         i++
+//     }
+
+//     while(j < ar1.length) {
+//         newArr.push(ar2[j])
+//         j++
+//     }
+//     return newArr
+// }
+
+// console.log(merge([1, 2, 3], [ 0, 2, 5, 7]))
 
 
-// console.log(mergeSort([1, 3, 7, 3, 2, 14, 12, 27, 33, 5, 1, 19]))
+console.log(mergeSort([1, 3, 7, 3, 2, 14, 12, 27, 33, 5, 1, 19]))
